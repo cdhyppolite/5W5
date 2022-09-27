9027bc75b9f382c45cafee07ac2ba8ccedd063fa
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-09-27 03:28:53 -0400

    sprint1 - police H2 et autres

--- category.php
+++ category.php
@@ -15,42 +15,29 @@
         </nav>
         
             <?php if (have_posts()):
-                while (have_posts()): the_post(); ?>
-                <?php
-                    $titre = get_the_title();
-                    $titreFiltreCours = substr($titre, 7, -6);
-                    $nbHeures = substr($titre, -6);
-                    $codeCours = substr($titre, 0,7);
-                    $descCours = get_the_excerpt();
-                    //----Afficher Bordu cours-----
-                    $etat = "a-faire";
-                    $reussi = array("1J1", "2J2", "3J3", "1W1","2W2","3W3","1M1","1M2","2M3","2M4", "3M5","3C1",);
-                    $encours = array("4J4","4PA","4W4","4C2",);
-                    if (in_array(substr($codeCours, 4,3), $reussi)) { 
-                        $etat = "reussi";
-                    } else if (in_array(substr($codeCours, 4,3), $encours)) { 
-                        $etat = "en-cours";
-                    }
-                    //-----------------------------
-                ?>
-                
-                
-                <?php
-                    if ( (isset($_GET["session"]) && $_GET["session"] == $codeCours[4]) || (!isset($_GET["session"]))) :?>
+                    while (have_posts()): the_post(); ?>
+                    <?php
+                        $titre = get_the_title();
+                        $titreFiltreCours = substr($titre, 7, -6);
+                        $nbHeures = substr($titre, -6);
+                        $codeCours = substr($titre, 0,7);
+                        $descCours = get_the_excerpt();
+                    ?>
+                    
+                    <?php
+                        if ( (isset($_GET["session"]) && $_GET["session"] == $codeCours[4]) || (!isset($_GET["session"]))) :?>
 
-                <article style="background-image: url('<?= get_the_post_thumbnail_url(); ?>');"
-                    class="formation__cours <?= $etat; ?>">
-                    <?php /*the_post_thumbnail('thumbnail'); */?>
-                    <h3 class="cours__titre"> <a href="<?= get_permalink(); ?>"> <?= $titreFiltreCours; ?> </a></h3>
-                    <!-- <div class="cours__nbre-heure"><?= $nbHeures; ?></div> -->
-                    <!-- <p class="cours__code"><?= $codeCours; ?> </p> -->
-                    <!-- <div class="cours_etat"></div> -->
-                    <!-- <p class="cours__desc"> <?= $descCours; ?></p> -->
-                </article>
+                        <article style="background-image: url('<?= get_the_post_thumbnail_url(); ?>');"
+                            class="formation__cours <?= $etat; ?>">
+                            <?php /*the_post_thumbnail('thumbnail'); */?>
+                            <h3 class="cours__titre"> <a href="<?= get_permalink(); ?>"> <?= $titreFiltreCours; ?> </a></h3>
+                            <!-- <div class="cours__nbre-heure"><?= $nbHeures; ?></div> -->
+                            <!-- <p class="cours__code"><?= $codeCours; ?> </p> -->
+                            <!-- <p class="cours__desc"> <?= $descCours; ?></p> -->
+                        </article>
 
-                <?php endif ?>
-
-                
+                    <?php endif ?>
+                    
                     <?php endwhile ?>
                 <?php endif ?>
         </div>
