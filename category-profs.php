1919d1c754216078dde10e02cbe77ae860c01009
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-10-20 15:27:09 -0400

    sprint2-début page profs

---
+++ category-profs.php
@@ -0,0 +1,27 @@
+<?php get_header();
+        $count =0; 
+        $imageBlank = get_bloginfo('template_directory')."/images/blank.jpg"; ?>
+<main class="site__main">
+    <section class="listeProfs">
+        <h2 class="listeProfs__titre">Liste des enseignants</h2>
+        <div class="listeProfs__liste">
+
+            <?php if (have_posts()):
+                    while (have_posts()): the_post(); $count++?>
+            <?php $nomProf = get_the_title(); ?>
+
+            <article class="prof">
+                <div class="prof__img">
+                    <img src="<?php if (has_post_thumbnail()) { echo get_the_post_thumbnail_url(); } else { echo $imageBlank; } ?>" alt="">
+                </div>
+                <div class="prof__texte">
+                    <h3 class="prof__titre"> <a href="<?= get_permalink(); ?>"> <?= $nomProf; ?> </a> </h3>
+                </div>
+            </article>
+
+                <?php endwhile ?>
+            <?php endif ?>
+        </div>
+    </section>
+</main>
+<?php get_footer() ?>
