fc8aeeb77bf907b1a35134cdbb85288e0c9f4b1a
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-11-01 10:14:03 -0400

    changements function.php

--- functions.php
+++ functions.php
@@ -1,41 +1,37 @@
 <?php
-/**
- * Inclure fichiers css
- */
-function _5w5_enqueue()
-{
-    wp_enqueue_style('5w5-style-css', get_stylesheet_uri());
-    wp_enqueue_style('5w5-google-fonts', 'https://fonts.googleapis.com/css2?family=Neonderthaw&display=swap', false);
-
-    //JavaScript
-    // wp_register_script('4w4-js', get_template_directory_uri() . '/js/menu.js', array(), filemtime(get_template_directory() . '/js/menu.js'), true);
-    // wp_enqueue_script('menuJS');
+//--------------------------------------------------------- Fichiers à inclure
+function e4_5w5_enqueue(){
+    
+    wp_enqueue_style('5w5-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
+    wp_register_script('5w5-menuJS', get_template_directory_uri() . '/js/menu.js', array(), filemtime(get_template_directory() . '/js/menu.js'), true);
+    
+    wp_enqueue_script('5w5-menuJS');
 }
-add_action('wp_enqueue_scripts', '_5w5_enqueue');
+
+add_action("wp_enqueue_scripts", "e4_5w5_enqueue");
 
 /* -------------------------------------------------- Enregistré le menu */
-function cidw_5w5_register_nav_menu()
-{
+function e4_5w5_register_nav_menu(){
     register_nav_menus(array(
-        'menu_principal' => __('Menu principal', 'cidw_5w5') ,
-        'menu_footer' => __('Menu footer', 'cidw_5w5') ,
-        'menu_categorie_cours' => __('Menu Cours', 'cidw_5w5')
+        'menu_principal' => __('Menu principal', 'e4_5w5') ,
+        'menu_footer' => __('Menu footer', 'e4_5w5') ,
+        'menu_categorie_cours' => __('Menu Cours', 'e4_5w5')
     ));
 }
-add_action('after_setup_theme', 'cidw_5w5_register_nav_menu', 0);
+add_action( 'after_setup_theme', 'e4_5w5_register_nav_menu', 0 );
+
 /****************** - Add Thème support -***************** */
- function cidw_5w5_add_theme_support() {
+ function e4_5w5_add_theme_support() {
     add_theme_support('post-thumbnails');
     add_theme_support('custom-logo', array(
         'height' => 100,
         'width' => 100,
     ));
-    // add_theme_support('block-templates');
  }
-add_action( 'after_setup_theme', 'cidw_5w5_add_theme_support' );
+add_action( 'after_setup_theme', 'e4_5w5_add_theme_support' );
 
 // Afficher les cours en ordre de session.
-function cidw_5w5_pre_get_posts(WP_Query $query)
+function e4_5w5_pre_get_posts(WP_Query $query)
 {
     if (is_admin()
     || !$query ->is_main_query()
@@ -53,12 +49,12 @@ function cidw_5w5_pre_get_posts(WP_Query $query)
         return $query;
     }
 }
-function cidw_5w5_query_vars($params)
+function e4_5w5_query_vars($params)
 {
     $params[] = "cletri";
     $params[] = "ordre";
     return $params;
 }
-add_action('pre_get_posts', 'cidw_5w5_pre_get_posts');
-add_filter('query_vars', 'cidw_5w5_query_vars');
+add_action('pre_get_posts', 'e4_5w5_pre_get_posts');
+add_filter('query_vars', 'e4_5w5_query_vars');
 ?>
--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,13 @@
-console.log("MENU.JS");
+(function() {
+    // console.log("MENU.JS");
+    for (let i = 0; i < liMenuPrincipal.length; i++) {
+        liMenuPrincipal[i].innerHTML += '<div class="colone"></div>';
+    }
+}(
+
+))
 
 let menuPrincipal = document.getElementById("menu-principal");
 let liMenuPrincipal = menuPrincipal.querySelectorAll("li");
 
 // console.log(liMenuPrincipal.length);
-
-for (let i = 0; i < liMenuPrincipal.length; i++) {
-    liMenuPrincipal[i].innerHTML += '<div class="colone"></div>';
-}
