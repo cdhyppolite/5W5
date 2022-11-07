3766fc619fa6973920dcd61ce36abcecbae82f2b
Carl-David Hyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-11-07 16:02:52 -0500

    SP3 - Début page avenir

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,6 @@ let liMenuPrincipal = menuPrincipal.querySelectorAll("li");
 
 // console.log(liMenuPrincipal.length);
 
-(function() {
-    // console.log("MENU.JS");
-    for (let i = 0; i < liMenuPrincipal.length; i++) {
+for (let i = 0; i < liMenuPrincipal.length; i++) {
         liMenuPrincipal[i].innerHTML += '<div class="colone"></div>';
     }
-}(
-
-))
--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php get_header() ?>
-<main class="site__main">
+<main class="site__main _page">
     <!-- Afficher titre et contenu -->
     <h1><?php the_title() ?></h1>
     <?php the_content() ?>
