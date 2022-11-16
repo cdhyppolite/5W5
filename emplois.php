b3c08365f03c7e5a13227216d3458cd845fa853e
Zachary Arsenault <89539887+TheFrenchBuck@users.noreply.github.com>
2022-11-16 11:09:20 -0500

    Add files via upload
    Ajouer des textes dans les pages emplois , stages et université

--- emplois.php
+++ emplois.php
@@ -11,50 +11,9 @@ get_header();
     <h1><?php the_title() ?></h1>
     <Section class="emplois">
         <div class="imgEmplois"> <img src="http://localhost/2022_4w4/wp-content/uploads/2022/11/GrossEmploi.jpg" alt=""></div>
-        <div class="descEmplois"> <p>Après votre technique, vous avez l’option de rentrer directement dans le marché du travail, dans des rôles tels que:</p>
-<ul class="listeEmplois">
-    <li>Spécialiste en expérience utilisateur (UX)</li>
-    <li>Intégrateur multimédia</li>
-    <li>Concepteur de niveau de jeux vidéos</li>
-    <li>Développeur d’applications mobiles</li>
-    <li>Animateur 2D/3D</li>
-    <li>Chargé de projets numériques</li>
-    <li></li>
-</ul>
-
-
-
-
-
-
-Designer graphique et infographiste
-Monteur vidéo
-Développeur Front-end
-Programmeur de jeux videos
-Artiste 3D
-
-<p> taux de placement en emploi de 83,3 %*</p>
-</div>
-    </Section>
-
-</main>
-
-
-<?php get_footer() ?>
-<?php 
- /* 
- Template Name:Emplois
- */
-get_header(); 
-
-
-?>
-<main class="site__main _page">
-    <!-- Afficher titre et contenu -->
-    <h1><?php the_title() ?></h1>
-    <Section class="emplois">
-        <div class="imgEmplois"> <img src="http://localhost/2022_4w4/wp-content/uploads/2022/11/GrossEmploi.jpg" alt=""></div>
-        <div class="descEmplois"> <p>Après votre technique, vous avez l’option de rentrer directement dans le marché du travail avec un taux de placement en emploi de 83,3 % ! Dans des rôles tels que:</p>
+        <div class="descEmplois">
+            <h2>Après votre technique</h2>
+             <p>Après votre technique, vous avez l’option de rentrer directement dans le marché du travail, dans des rôles tels que:</p>
 <ul class="listeEmplois">
     <li>Spécialiste en expérience utilisateur (UX)</li>
     <li>Intégrateur multimédia</li>
@@ -64,12 +23,13 @@ get_header();
     <li>Chargé de projets numériques</li>
     <li>Designer graphique et infographiste</li>
     <li>Monteur vidéo</li>
-    <li>Développeur Front-end</li>
     <li>Programmeur de jeux videos</li>
+    <li>Développeur Front-end</li>
     <li>Artiste 3D</li>
+    
 </ul>
-
-
+<h2>Placement</h2>
+<p> La technique taux de placement en emploi de 83,3 %*</p>
 </div>
     </Section>
 
