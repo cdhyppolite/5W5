c1daede584f807ecfb01438091f0300e9f2b11c1
Joannye Fontaine <joannye.fontaine@gmail.com>
2022-11-21 15:07:25 -0500

    Intégratrion Vidéo

--- front-page.php
+++ front-page.php
@@ -4,9 +4,9 @@
 
 <main class="site__main">
     <!-- Afficher la vidéo -->
-    <iframe width="1280" height="720" 
-        src="https://www.youtube.com/embed/YOo92SwTZEs?<?= $YT_Params; ?>">
-    </iframe>
+    <div class="iframe-container">
+        <iframe src="https://www.youtube.com/embed/EwQ6_gHmx5U?autoplay=1&loop=1&playlist=EwQ6_gHmx5U" width="1280" height="720" title="Presentation3" frameborder="0" allowfullscreen></iframe>
+    </div>
     <p>Bienvenue sur le site du collège de Maisonneuve!</p>
 </main>
 <?php get_footer() ?>
