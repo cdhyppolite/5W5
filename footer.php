8f4502a97e7585406e8fdbf8c1843c3c9918e3a7
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-11-26 21:01:20 -0500

    SP4 - Affichage du titre dans "title" selon la pagr

--- footer.php
+++ footer.php
@@ -1,14 +1,15 @@
 <label class="boiteTransparent" for="chk-burger"></label>
 <footer class="site__footer">
     <div class="site__footer__conteneur">
-        <div>1</div>
-        <div class="site__adresse">
+        <div class="site__footer__adresse">
             <h4>Adresse</h4>
             <p>3800 Rue Sherbrooke E, Montréal, QC</p>
             <p>H1X 2A2</p>
             <p>(514) 254-7131</p>
         </div>
-        <div>3</div>
+        <div>
+            <h4>Suivez-nous</h4>
+        </div>
         <div class="site__copyright">
             <h2>&copy; <?php echo date("Y"); ?> - TIM Maisonneuve - Tous droits réservés</h2>
         </div>
--- front-page.php
+++ front-page.php
@@ -1,12 +1,11 @@
-<?php get_header() ?>
-<!-- Paramètre de l'url de la vidéo YT -->
-<?php $YT_Params = "&loop=1&autoplay=1&mute=1&controls=0" ?>
+<?php get_header(); ?>
 
 <main class="site__main">
-    <!-- Afficher la vidéo -->
+    <?php // Afficher la vidéo ?>
     <div class="iframe-container">
         <iframe src="https://www.youtube.com/embed/EwQ6_gHmx5U?autoplay=1&loop=1&playlist=EwQ6_gHmx5U" width="1280" height="720" title="Presentation3" frameborder="0" allowfullscreen></iframe>
     </div>
     <p>Bienvenue sur le site du collège de Maisonneuve!</p>
 </main>
+
 <?php get_footer() ?>
