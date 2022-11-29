f45187c4350a9169da8e5dd47571670fed13c6cd
Zachary Arsenault <89539887+TheFrenchBuck@users.noreply.github.com>
2022-11-28 20:42:56 -0500

    Ajouter effet lumineux background

--- header.php
+++ header.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php if(is_front_page()) echo "Accueil"; else wp_title(''); ?> | <?php bloginfo('name'); ?></title>
-    
+    <title><?= get_bloginfo('name'); ?></title>
+    <!-- Police Last Enter -->
+    <link rel="preload" href="<?php echo (get_bloginfo('template_directory')."/fonts/LasEnterPersonalUseOnly.woff2") ?>" as="font" type="font/woff2" crossorigin>
+    <!-- Police Neon Blitz -->
+    <link rel="preload" href="<?php echo (get_bloginfo('template_directory')."/fonts/Neonblitz.woff2") ?>" as="font" type="font/woff2" crossorigin>
+    <!-- Police Neon Sans -->
+    <link rel="preload" href="<?php echo (get_bloginfo('template_directory')."/fonts/NeonSans.woff2") ?>" as="font" type="font/woff2" crossorigin>
     <!-- Police Lobster -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lobster">
     <?php wp_head(); ?>
@@ -17,7 +23,8 @@
             <h1 class="header__principal"><?= get_bloginfo('name'); ?></h1>
             <h6 class="header__secondaire"> <?php echo (get_bloginfo('description', 'display')); ?> </h6>
         </a>
+        
     </header>
-    
+    <div class="effet_background"></div>
     <!-- Menu avec la liste des cours -->
 <?php include('inclusions/menuBarreCours.php');?>
