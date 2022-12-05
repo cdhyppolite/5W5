8554e298c136b45419b8e03870612ab5384374dc
Carl-David Hyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-12-05 16:54:03 -0500

    SP4 - Ouvrir lien footer dans autre page

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,20 @@
 // Ce fichier permet d'ajouter des divs dans chaque élément du menu principal
-let menuPrincipal = document.getElementById("menu-principal");
+let d = document;
+let menuPrincipal = d.getElementById("menu-principal");
 let liMenuPrincipal = menuPrincipal.querySelectorAll("li");
-let menuBurger = document.getElementById('burger');
-let boiteTransparent = document.querySelector('.boiteTransparent');
-let page = document.querySelector('html');
-let btnMenuBurger = document.querySelector('#chk-burger');
+let menuBurger = d.getElementById('burger');
+let boiteTransparent = d.querySelector('.boiteTransparent');
+let page = d.querySelector('html');
+let btnMenuBurger = d.querySelector('#chk-burger');
 let menuOuvert = false;
+let liSidebarFooter = d.querySelectorAll(".wp-block-social-link-anchor");
 
 for (let i = 0; i < liMenuPrincipal.length; i++) {
     liMenuPrincipal[i].innerHTML += '<div class="menu-item__ligne"></div>';
+}
+
+for (let i = 0; i < liSidebarFooter.length; i++) {
+    liSidebarFooter[i].setAttribute('target', '_blank');
 }
 
 (function() {
--- sidebar-reseaux_sb.php
+++ sidebar-reseaux_sb.php
@@ -1,4 +1,4 @@
-<div id="sidebar-resaeux" class="site__footer__reseaux">
+<div id="sidebar-reseaux" class="site__footer__reseaux">
     <h4>Suivez-nous</h4>
     <?php dynamic_sidebar( 'reseaux_sb' ); ?>
 </div>
