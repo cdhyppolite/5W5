5133382979be22906b888f484bfd8efbab803f14
Carl-David Hyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-12-12 16:53:06 -0500

    SP4 - amélioration taille page cours

--- category-cours.php
+++ category-cours.php
@@ -16,11 +16,11 @@
                         unset($_GET["session"]); // Détruire la variable
                     }
                 ?>
-                <a href="?" <?php if (!isset($_GET["session"])) echo 'class="active"' ?>>Tous</a>
+                <a title="Toutes les sessions" href="?" <?php if (!isset($_GET["session"])) echo 'class="active"' ?>>Tous</a>
                 <?php // Boucle ?>
                 <?php for ($nbSessions = 1; $nbSessions <= 6; $nbSessions++) : ?>
-                <a href="?session=<?= $nbSessions; ?>"
-                    <?= (isset($_GET["session"]) && ($_GET["session"]==$nbSessions)) ? 'class="active"' :""; ?>>Session
+                <a title="Session <?= $nbSessions; ?>" href="?session=<?= $nbSessions; ?>"
+                    <?= (isset($_GET["session"]) && ($_GET["session"]==$nbSessions)) ? 'class="active"' :""; ?>>
                     <?= $nbSessions; ?></a>
                 <?php endfor; ?>
             </nav>
