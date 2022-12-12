1e1b86650b7168eec7f460fb974172e294c3ceb4
Emmanuel Thomas <thomas21emmanuel@gmail.com>
2022-12-12 16:54:36 -0500

    Margin + tailles page cours

--- template-profs.php
+++ template-profs.php
@@ -17,6 +17,7 @@
 
       <div class="unProf__infos">
             <!-- Afficher la photo de l'enseignant -->
+            <p>GMM</p>
             <img src="<?php if (has_post_thumbnail()) { echo get_the_post_thumbnail_url(); } else { echo $imageBlank; } ?>" alt="">
          <div class="unProf__desc">
             <p><?= $description; ?></p>
