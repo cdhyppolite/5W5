f9b449c8053d9b754620af34311c5b518e7b63df
Joannye Fontaine <joannye.fontaine@gmail.com>
2022-12-12 14:10:46 -0500

    section prof

--- template-profs.php
+++ template-profs.php
@@ -19,16 +19,12 @@
             <!-- Afficher la photo de l'enseignant -->
             <img src="<?php if (has_post_thumbnail()) { echo get_the_post_thumbnail_url(); } else { echo $imageBlank; } ?>" alt="">
          <div class="unProf__desc">
-            <p><?= $description; ?></p>
-            <p class="unProf__anciennete">Depuis combien de temps enseignez-vous cette matière ? <?php the_field('annee'); ?></p>
-            <p class="unProf__expertise">Quel est votre domaine d'expertise (en TIM et/ou ailleurs) ?  <?php the_field('expertise'); ?></p>
-            <p class="unProf__outils">Quels sont vos outils de prédilection pour transmettre cette matière (logiciels, concepts, philosophie, etc.)? <?php the_field('outils'); ?></p>
-            <p class="unProf__interieur_TIM">À l'intérieur de TIM, y a-t-il un ou des projets desquels vous êtes particulièrement fier/fière? Lesquels? <?php the_field('interieur_TIM'); ?></p>
-            <p class="unProf__exterieur_TIM">En dehors de TIM, y a-t-il ou ou des projets desquels vous êtes particulièrement fier/fière ? Lesquels? <?php the_field('exterieur_TIM'); ?></p>
-            <p class="unProf__passion">Qu'est-ce qui vous passionne particulièrement dans le multimédia? <?php the_field('passion'); ?></p>
-            <p class="unProf__matiere_preferee">Votre matière préférée à enseigner? <?php the_field('matiere_preferee'); ?></p>
-            <p class="unProf__matiere_difficile">La matière pour laquelle vous n’avez aucune compétence ? <?php the_field('matiere_difficile'); ?></p>
-            <p class="unProf__one_liner">Si vous étiez un personnage de fiction, quel serait votre « one-liner »? <?php the_field('one_liner'); ?></p>
+            <p class = "description"><?= $description; ?></p>
+            <br>
+            <p class="unProf">Depuis combien de temps enseignez-vous cette matière ?</p> <p><?php the_field('annee'); ?></p>
+            <p class="unProf">Quel est votre domaine d'expertise (en TIM et/ou ailleurs) ?</p> <p><?php the_field('expertise'); ?></p>
+            <p class="unProf">Quels sont vos outils de prédilection pour transmettre cette matière (logiciels, concepts, philosophie, etc.)?</p> <p><?php the_field('outils'); ?></p>
+            <p class="unProf">Qu'est-ce qui vous passionne particulièrement dans le multimédia?</p> <p><?php the_field('passion'); ?></p>
          </div>
       </div>
    </section>
